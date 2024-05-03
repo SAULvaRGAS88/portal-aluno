7e3cd8baaa296679043761ee87a6878eb5655853
saulvargas88 <saulvargascosta@hotmail.com>
2024-05-03 00:11:26 -0300

    path deploy

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 10000),
   app: {
     keys: env.array('APP_KEYS'),
   },
@@ -8,6 +8,6 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
   admin: {
-    path: '/dashboard', 
+    path: 'https://portal-aluno.onrender.com/', 
   },
 });
